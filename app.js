0e6a4cb05b4e35c431a7ce846cdbd99315fd9976
Francisco <franperitano1997@gmail.com>
2022-07-18 16:20:46 -0300

    js

---
+++ app.js
@@ -0,0 +1,9 @@
+let nombre = prompt ("Ingrese su nombre");
+
+alert("Bienvenido loco o loca ;)");
+
+let apellido = prompt ("te falto el apellido loquito/a");
+alert("Ahora si jeje");
+
+let suma = prompt ("Vamos a revisar tu IQ con una suma extremadamente dificil cuanto es 1 + 1?")
+alert("Dijiste 2? pues no, segun ella es 3 ¡Segui Participando! xd")
